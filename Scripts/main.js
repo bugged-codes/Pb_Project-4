018283807cf27bde654edf2db298d227ad5e9423
bugged-codes <vipul_49@outlook.com>
2022-12-01 06:05:56 +0530

    Project Started!

---
+++ Scripts/main.js
@@ -0,0 +1,10 @@
+// function time() {
+//     let dt = new Date();
+//     let hr = dt.getHours();
+//     let min = dt.getMinutes();
+//     let sec = dt.getSeconds();
+
+//     console.log(hr, min, sec);
+//     setInterval(time, 1000);
+// }
+console.warn("hi i am vipul");
